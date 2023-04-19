a5af580c1e79e2df57f30278763d767930b9f4d8
hs96wings <hs96wings@gmail.com>
2023-04-19 15:26:27 +0900

    add 구현, get

--- src/main/java/com/ll/exam/MyArrayList.java
+++ src/main/java/com/ll/exam/MyArrayList.java
@@ -1,6 +1,7 @@
 package com.ll.exam;
 
 public class MyArrayList<T>{
+    private String[] data = new String[2];
     private int size = 0;
     public int size() {
         return size;
@@ -8,11 +9,13 @@ public class MyArrayList<T>{
 
     // ArrayList의 add는 무조건 true를 반환해주어야 함
     public boolean add(String element) {
+        data[size] = element;
+
         size++;
         return true;
     }
 
     public String get(int index) {
-        return "포도";
+        return data[index];
     }
 }
